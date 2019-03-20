ed056477feb1178ae0dbde1a4d4bd064d3bf0f64
Zsolt Gajdács <33376024+ZsoltGajdacs@users.noreply.github.com>
2019-03-20 20:39:05 +0100

    Moved out serialization from taskbar window

---
+++ BlinkReminder/Helpers/Serializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlinkReminder.Helpers
+{
+    internal static class Serializer
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Serializes the given object to the given path
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="serializePath"></param>
+        internal static void SerializeObj(Object o, string serializePath, string parentDir)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(parentDir);
+
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(serializePath, FileMode.Create, FileAccess.Write);
+
+                formatter.Serialize(stream, o);
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Serialization failed!");
+            }
+        }
+    }
+}
